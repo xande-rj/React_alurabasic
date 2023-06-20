f8dfa37a8b127c79a64a42bcab109b4f6e97896a
Alexandre <alexandresargitario@gmail.com>
2023-06-20 15:20:46 -0300

    primeiro commit

---
+++ src/app/componetes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import "./Banner.css"
+function Banner (){
+    return(
+        <header className="banner">
+            <img src="/Imagens/banner.png" alt="Banner"/>
+        </header>
+    )
+}
+
+export default Banner
