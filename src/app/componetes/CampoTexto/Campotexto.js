5c7b566a05aaa36491ee5e6d526811b88dedd8ce
Alexandre <alexandresargitario@gmail.com>
2023-06-21 09:51:59 -0300

    adição do campo texto

---
+++ src/app/componetes/CampoTexto/Campotexto.js
@@ -0,0 +1,12 @@
+// funciona a mesma coisa que o banner, a escolha e sua.
+const CampoTexto = ()=>{
+    <div>
+        <label>
+            Nome 
+            <input/>
+        </label>
+    </div>
+    
+}
+
+export default CampoTexto
