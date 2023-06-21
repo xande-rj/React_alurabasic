4bbcdd7ea4bf84ca1460146edfe1e5f5ec9b0a91
Alexandre <alexandresargitario@gmail.com>
2023-06-21 10:06:06 -0300

    Adicionando campo texto a page

--- src/app/componetes/CampoTexto/Campotexto.js
+++ src/app/componetes/CampoTexto/Campotexto.js
@@ -1,12 +1,13 @@
 // funciona a mesma coisa que o banner, a escolha e sua.
 const CampoTexto = ()=>{
-    <div>
+   return(
+   <div>
         <label>
             Nome 
             <input/>
         </label>
     </div>
-    
+   )
 }
 
 export default CampoTexto
