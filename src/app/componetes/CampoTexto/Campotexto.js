a8b766a3300ba480a4603049ad1618fda50cee71
Alexandre <alexandresargitario@gmail.com>
2023-06-21 10:38:32 -0300

    Adicionando props e modificando

--- src/app/componetes/CampoTexto/Campotexto.js
+++ src/app/componetes/CampoTexto/Campotexto.js
@@ -1,12 +1,16 @@
 import "./Campotexto.css"
 // funciona a mesma coisa que o banner, a escolha e sua.
-export const CampoTexto = ()=>{
+export const CampoTexto = (props)=>{
+    // react trabalha com props que recebe do da page, que nesse caso e a label
+    // O props e um variavel que significa propriedade e pode ser qualquer coisa 
+
+    const propsModificado = `${props.placeholder}...`
    return(
    <div className="campo-texto">
         <label>
-            Nome 
-            <input placeholder="Digite seu nome"/>
+            {props.label}
         </label>
+        <input placeholder = {propsModificado}/>
     </div>
    )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div>
     <Banner/>
-    <CampoTexto/>
+    <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
+    <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
+    <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem"/>
     </div>
   )
 }
