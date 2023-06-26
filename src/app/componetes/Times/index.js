98b6b431c1503008515607527374f7bd1db13365
Alexandre <alexandresargitario@gmail.com>
2023-06-26 14:55:40 -0300

    adição do time

---
+++ src/app/componetes/Times/index.js
@@ -0,0 +1,11 @@
+import "./Time.css"
+
+const Times = (props) =>{
+    return(
+        <section className="times">
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Times
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import Banner from './componetes/Banner'
 import Formulario from './componetes/Formulario/Formulario'
+import Times from './componetes/Times';
 
 export default function Home() {
   const [colaboradores, Setcolaboradores] =useState([])
@@ -15,6 +16,9 @@ export default function Home() {
     <div>
     <Banner/>
     <Formulario ColaboradorCadastrado = {colaborador =>NovoColaboradorAdicionado(colaborador)}/>
+    <Times nome="Programação"/>
+    <Times nome="Design"/>
+    <Times nome="Devoups"/>
     </div>
   )
 }
